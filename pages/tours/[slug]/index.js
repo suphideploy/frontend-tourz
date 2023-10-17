4f3d81264de28fe14bb161b2b401802dceaf8cbb
suphideploy <chingchimplee@gmail.com>
2023-10-18 01:40:17 +0700

    Edit Slug

--- pages/tours/[slug]/index.js
+++ pages/tours/[slug]/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import TourSingle from '@/src/components/toursingle';
+import Layout from "@/src/components/layouts";
+import { GET_MENUS } from "@/src/queries/get-menus";
+import client from "@/src/apollo/client";
 
-function single() {
+function single({data}) {
   return (
+
     <div>
-    <TourSingle />
+       <Layout data={data}>
+       <div className=" ">
+           <TourSingle />
+        </div>
+        
+        </Layout>
+            
     </div>
   )
 }
 
 export default single
+
+export async function getStaticProps(context){
+    const {data, loadng, networkStatus} = await client.query({
+  
+      query: GET_MENUS
+    });
+  
+    console.warn('data', data);
+  
+    return {
+            props: {
+              data: {
+                menus: {
+                  headerMenus: data?.headerMenus?.edges,
+                  footerMenus: data?.footerMenus?.edges
+            }
+        }
+           
+      }
+    }
+  
+  }
